a40d9a1e5580131b4db91247e6d579aaa7e95365
Mario Lukas <info@mariolukas.de>
2019-02-08 20:05:20 +0100

    changed project structure

--- Version.h
+++
@@ -1,1 +0,0 @@
-#define BUILD_VERSION        "v.1.1 b-20181123"
--- motor_controls.h
+++
@@ -1,9 +0,0 @@
-#include "ScannerConfig.h"
-#include <AccelStepper.h>
-
-extern int total_photos;
-extern int current_progress;
-extern bool stop_program;
-
-void initialize_motors();
-void start_motor_program(byte program);
